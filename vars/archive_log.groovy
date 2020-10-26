e7769794359b79901dd031eb32fc303c35e84df2
Ray Ng <ray.ng@gradeous.com.au>
2020-10-27 10:19:40 +1100

    add container name argument

--- vars/archive_log.groovy
+++ vars/archive_log.groovy
@@ -1,13 +1,13 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String name = 'jenkins') {
   script {
     withCredentials([usernamePassword(credentialsId: 'azure-storage', usernameVariable: 'AZURE_STORAGE_ACCOUNT', passwordVariable: 'AZURE_STORAGE_KEY')]) {
       def log = currentBuild.rawBuild.getLog()
       writeFile(file: 'buildlog.txt', text: log)
-      def data = readFile(file: 'buildlog.txt')
-      sh (script: 'az storage container create -n jenkinsarchivelogs', returnStdout: true)
-      sh (script: 'az storage blob upload -c jenkinsarchivelogs -f ./buildlog.txt -n ${JOB_NAME}/${BUILD_NUMBER}.txt', returnStdout: true)
+      env.CONTAINER = name
+      sh (script: 'az storage container create -n ${CONTAINER}', returnStdout: true)
+      sh (script: 'az storage blob upload -c ${CONTAINER} -f ./buildlog.txt -n ${JOB_NAME}/${BUILD_NUMBER}.txt', returnStdout: true)
     }
   }
 }
