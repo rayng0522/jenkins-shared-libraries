c8a531000a5dfb7361d71e7f139ddfdad4d49ef6
Ray Ng <ray.ng@gradeous.com.au>
2020-11-03 20:27:11 +1100

    move def value inside call function

--- vars/archive_log.groovy
+++ vars/archive_log.groovy
@@ -1,19 +1,18 @@
 #!/usr/bin/env groovy
-def VAULT_ADDR = 'https://vault.pru.intranet.asia'
-def VAULT_APPROLE = 'apr-rtsre-all-admin-jenkinshcf'
-def configuration = [
-    $class: 'VaultConfiguration',
-    vaultUrl: "${VAULT_ADDR}",
-    vaultCredentialId: "${VAULT_APPROLE}"
-]
-def secrets = [
-    [path: 'kv2/sgrtss/nprd/dev/t3stan/stasgrtssdevaz1t3stan001', engineVersion: 2, secretValues: [
-        [envVar: 'AZURE_ACCOUNT_ID', vaultKey: 'storage_account_name'],
-        [envVar: 'AZURE_STORAGE_KEY', vaultKey: 'primary_key']]]
-]
-
 def call(String name = 'jenkins') {
   script {
+    def VAULT_ADDR = 'https://vault.pru.intranet.asia'
+    def VAULT_APPROLE = 'apr-rtsre-all-admin-jenkinshcf'
+    def configuration = [
+        $class: 'VaultConfiguration',
+        vaultUrl: "${VAULT_ADDR}",
+        vaultCredentialId: "${VAULT_APPROLE}"
+    ]
+    def secrets = [
+        [path: 'kv2/sgrtss/nprd/dev/t3stan/stasgrtssdevaz1t3stan001', engineVersion: 2, secretValues: [
+            [envVar: 'AZURE_ACCOUNT_ID', vaultKey: 'storage_account_name'],
+            [envVar: 'AZURE_STORAGE_KEY', vaultKey: 'primary_key']]]
+    ]
     // inside this block your credentials will be available as env variables
     withVault([configuration: configuration, vaultSecrets: secrets]) {
       def storage_account_name = env.AZURE_ACCOUNT_ID.split('/')
