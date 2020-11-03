2b963530c71ff33f509ce1756e3592ee08bcee50
Ray Ng <ray.ng@gradeous.com.au>
2020-11-03 20:18:14 +1100

    update vault configuration

--- vars/archive_log.groovy
+++ vars/archive_log.groovy
@@ -7,18 +7,21 @@ def VAULT_CONFIGURATION = [
     vaultCredentialId: "${VAULT_APPROLE}"
 ]
 def secrets = [
-    [path: 'kv2/sgrtss/nprd/dev/t3stan/stasgrtssdevaz1t3stan001', engineVersion: 1, secretValues: [
-        [envVar: 'AZURE_STORAGE_ACCOUNT', vaultKey: 'storage_account_name'],
+    [path: 'kv2/sgrtss/nprd/dev/t3stan/stasgrtssdevaz1t3stan001', engineVersion: 2, secretValues: [
+        [envVar: 'AZURE_ACCOUNT_ID', vaultKey: 'storage_account_name'],
         [envVar: 'AZURE_STORAGE_KEY', vaultKey: 'primary_key']]]
 ]
 
 def call(String name = 'jenkins') {
   script {
     // inside this block your credentials will be available as env variables
+    def configuration = VAULT_CONFIGURATION
     withVault([configuration: configuration, vaultSecrets: secrets]) {
+      def storage_account_name = env.AZURE_ACCOUNT_ID.split('/')
+      env.AZURE_STORAGE_ACCOUNT = storage_account_name.last()
       def log = currentBuild.rawBuild.getLog()
       writeFile(file: 'buildlog.txt', text: log)
-      env.CONTAINER = name
+      env.CONTAINER = 'jenkinsarchivelogs'
       sh (script: 'az storage container create -n ${CONTAINER}', returnStdout: true)
       sh (script: 'az storage blob upload -c ${CONTAINER} -f ./buildlog.txt -n ${JOB_NAME}/${BUILD_NUMBER}.txt', returnStdout: true)
     }
