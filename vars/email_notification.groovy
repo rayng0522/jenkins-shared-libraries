d2353772a19094fd96cc44411479c3cf988d4412
Ray Ng <ray.ng@gradeous.com.au>
2020-11-05 11:04:38 +1100

    fix indentation

--- vars/email_notification.groovy
+++ vars/email_notification.groovy
@@ -3,34 +3,34 @@
 def call(buildStatus, emailRecipients, customBody="", hasApproval=false) {
   try {
 
-  def icon = "✅"
-  def statusSuccess = true
-  // def logUrl        = env.BUILD_URL + 'consoleText'
-  def logUrl        = env.RUN_DISPLAY_URL
-  def approvalUrl   = env.BUILD_URL + 'input'
-  def content       = customBody
+    def icon = "✅"
+    def statusSuccess = true
+    // def logUrl        = env.BUILD_URL + 'consoleText'
+    def logUrl        = env.RUN_DISPLAY_URL
+    def approvalUrl   = env.BUILD_URL + 'input'
+    def content       = customBody
 
-  if (content == "") {
-    content = "\"${env.JOB_NAME}\" build: ${env.BUILD_NUMBER}\n\nView the log at:\n $logUrl"
-  }
+    if (content == "") {
+      content = "\"${env.JOB_NAME}\" build: ${env.BUILD_NUMBER}\n\nView the log at:\n $logUrl"
+    }
 
-  if(buildStatus != "SUCCESSFUL") {
-    icon = "❌"
-    body = "Job Failed - $content"
-    statusSuccess = false
-  }
-  else {
-    if (hasApproval) {
-      body = "Job Success - $content\n\nApprove this deployment:\n$approvalUrl"
+    if(buildStatus != "SUCCESSFUL") {
+      icon = "❌"
+      body = "Job Failed - $content"
+      statusSuccess = false
     }
     else {
-      body = "Job Success - $content"
+      if (hasApproval) {
+        body = "Job Success - $content\n\nApprove this deployment:\n$approvalUrl"
+      }
+      else {
+        body = "Job Success - $content"
+      }
     }
-  }
 
-  mail to: emailRecipients.join(","),
-       subject: "${icon} [ ${env.JOB_NAME} ] [${env.BUILD_NUMBER}] - ${buildStatus} ",
-       body: body
+    mail to: emailRecipients.join(","),
+         subject: "${icon} [ ${env.JOB_NAME} ] [${env.BUILD_NUMBER}] - ${buildStatus} ",
+         body: body
 
   } catch (e){
       println "ERROR SENDING EMAIL ${e}"
--- vars/teams_notification.groovy
+++ vars/teams_notification.groovy
@@ -3,33 +3,33 @@
 def call(buildStatus, customBody="", hasApproval=false) {
   try {
 
-  def icon = "✅"
-  def message       = customBody
-  def color         = "05b222"
+    def icon = "✅"
+    def message       = customBody
+    def color         = "05b222"
 
-  if(buildStatus != "SUCCESSFUL") {
-    icon    = "❌"
-    status  = "Job Failed"
-    color   = "d00000"
-  }
-  else {
-    if (hasApproval) {
-      def approvalUrl   = env.BUILD_URL + 'input'
-      status            = "Job Success - Approve this deployment: [click me]($approvalUrl)"
+    if(buildStatus != "SUCCESSFUL") {
+      icon    = "❌"
+      status  = "Job Failed"
+      color   = "d00000"
     }
     else {
-      status  = "Job Success"
+      if (hasApproval) {
+        def approvalUrl   = env.BUILD_URL + 'input'
+        status            = "Job Success - Approve this deployment: [click me]($approvalUrl)"
+      }
+      else {
+        status  = "Job Success"
+      }
     }
-  }
 
-  if (message == "") {
-    message = "${icon} started ${env.JOB_NAME} and Build# ${env.BUILD_NUMBER}"
-  }
+    if (message == "") {
+      message = "${icon} started ${env.JOB_NAME} and Build# ${env.BUILD_NUMBER}"
+    }
 
-  office365ConnectorSend message: message,
-                         status: status,
-                         color: color,
-                         webhookUrl: "${env.TEAMS_WEBHOOK_URL}"
+    office365ConnectorSend message: message,
+                           status: status,
+                           color: color,
+                           webhookUrl: "${env.TEAMS_WEBHOOK_URL}"
 
 
   } catch (e){
